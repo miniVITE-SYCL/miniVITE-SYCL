17b0a83f3f2ddf1f4c278e8b0bf8ce9260eb1b50
user5423 <user5423@localhost.localdomain>
2023-04-11 12:43:41 +0100

    Added notes and pseudocode on how to generate validation summary results

--- tests/validation.py
+++ tests/validation.py
@@ -38,6 +38,49 @@ class MiniviteCorrectnessValidator(MiniviteVariantTester):
         return formattedResults
 
     def analyse(self) -> None:
+        results = self._loadResults()
+        ## How are we going to analyse this?
+
+        ## The relevant groupings of inputs are (+ affect estimation):
+        ## - Misc Flags,  (No affect)
+        ## - MPI Flags, (No affect on None, and 1, affect on MPI rank > 1)
+        ## - DS Flags (Some affect)
+        ## - Graph Input Size (Large affect)
+        ## - Thread Count Env (Minor affect)
+        
+        ## We want to show 
+        ## - consistency between runs of the same configuration
+        ## - similarity of results between a identical configuration running on SYCL and OpenMP
+
+        ## We can define a test as the set
+        ## - ( [OpenMP/SYCL], [testConfiguration])
+
+        ## What we propose for testing both similarity and consistency:
+        ##  - for each attribute to focus on a_i
+        ##      - for each pair of test sets s1, and s2,
+        ##      - where a_i not in s_1, but a_i in s_2, and s_2 - a_i = s_1
+        ##          - we calculate the "distance" between these two sets
+        ##      - we then accumulate these "distance" for a single result
+        ##      - if s_1 or s_2 has an error, then we note it in the table
+
+        ## Which metrics do we wish to use?
+        ## - modularity only for result similarity
+        ## - modularity and iterations for result consistency
+
+        ## How do we calculate this distance?
+        ## https://stackoverflow.com/questions/61120822/calculating-the-total-distance-between-multiple-points
+        ## - The absolute values don't really mean too much unless they are close to 0
+        ## - The values do mean something in relation to other values
+
+        ## How can we shorten this?
+        ## - for less relevant or near identical results, we can group values up
+        ## - e.g. I expect the Misc Flags, Thread Count, and MPI Flags to make a negligble impact
+        ## - the DS flags have only two options
+        ## - We can easily limit / group up the graph input size into buckets based on similar result statistics
+        ##      - e.g. [1000-10000], [10000-10000]
+
+
+
         raise NotImplementedError()
 
 
@@ -45,6 +88,7 @@ def main():
     v = MiniviteCorrectnessValidator()
     v.run()
     v.analyse()
+
 
 
 if __name__ == "__main__":
